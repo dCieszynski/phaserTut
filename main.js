b54c355475cba8257b4d975562bb6454b1423c4d
dCieszynski <dCieszynski99@gmail.com>
2022-08-09 20:00:13 -0500

    Project Init

---
+++ main.js
@@ -0,0 +1,18 @@
+import Phaser from "phaser";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+const game = new Phaser.Game(config);
+
+const preload = () => {};
+const create = () => {};
+const update = () => {};
