47e43bbf052c263f6b7618e258b67936324bdbbb
dCieszynski <dCieszynski99@gmail.com>
2022-08-09 20:28:23 -0500

    Add player and collisions

--- main.js
+++ main.js
@@ -29,9 +29,11 @@ function preload() {
     frameWidth: 32,
     frameHeight: 48,
   });
-
-  let platforms = null;
 }
+
+let platforms = null;
+let player = null;
+
 function create() {
   this.add.image(400, 300, "sky");
   platforms = this.physics.add.staticGroup();
@@ -41,5 +43,32 @@ function create() {
   platforms.create(600, 400, "ground");
   platforms.create(50, 250, "ground");
   platforms.create(750, 220, "ground");
+
+  //Player
+  player = this.physics.add.sprite(100, 450, "dude");
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.physics.add.collider(player, platforms);
 }
 function update() {}
