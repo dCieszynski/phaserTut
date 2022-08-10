c10beb8aa15436f40fc57381661326b53869a2c1
dCieszynski <dCieszynski99@gmail.com>
2022-08-09 20:09:51 -0500

    Project reinit, change arrow functions to regular

--- main.js
+++ main.js
@@ -13,6 +13,17 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-const preload = () => {};
-const create = () => {};
-const update = () => {};
+function preload() {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("ground", "assets/platform.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+function create() {
+  this.add.image(400, 300, "sky");
+}
+function update() {}
